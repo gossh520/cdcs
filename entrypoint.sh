24c43a37c04666ce11dee1741f5301a6d85875f2
gossh520 <73437060+gossh520@users.noreply.github.com>
2020-11-26 19:42:39 +0800

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+curl -LO https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip
+unzip v2ray-linux-64.zip
+rm -rf ./v2ray-linux-64.zip
+rm -rf ./systemd
+rm -rf ./config.json
+rm -rf ./v2ctl
+rm -rf ./geosite.dat
+rm -rf ./vpoint_socks_vmess.json
+rm -rf ./vpoint_vmess_freedom.json
+cat << EOF > config.json
+{
+  "inbounds": [
+  {
+    "port": 8080,
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
+          "alterId": 0       
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+      "path": "/ws"
+      }     
+    }
+  }
+  ],
+  "outbounds": [
+  {
+    "protocol": "freedom",
+    "settings": {}
+  }
+  ]
+}
+EOF
+./v2ray
