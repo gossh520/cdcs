5eee0b3b31e5ef2c7b8728a3569a5a682804b81f
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-02-27 03:30:32 +0800

    Add files via upload

---
+++ jd/beauty.js
@@ -0,0 +1,12 @@
+//读取cookiejd数据并创建变量
+if (String($store.get('CookieJD2', 'raw')) != "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw') + "&" + $store.get('CookieJD2', 'raw');
+if (String($store.get('CookieJD2', 'raw')) == "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw');
+$exec('node ./scripts/jd_beauty.js', {
+  cwd: './script/Shell/',
+  env: {
+    JD_COOKIE: JD_COOKIE1
+  },
+  cb(data){
+    console.log(data)
+  }
+})
