b5148bc33e715ccdd1a88e226e1486e7d366babb
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-02-27 01:03:21 +0800

    Add files via upload

---
+++ gx.js
@@ -0,0 +1,5 @@
+// 以 object 方式指定下载目录
+$download('https://raw.githubusercontent.com/gossh520/cdcs/main/gx.sh', {
+  folder: '/elecV2P/script/Shell',
+  name: 'gx.sh'
+}).then(d=>console.log('文件已下载: ' + d)).catch(e=>console.error(e))
