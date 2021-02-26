3187491943579d9590c07c2b3a50f9b611d10566
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-02-27 03:55:45 +0800

    Update bean_home.js

--- jd/bean_home.js
+++ jd/bean_home.js
@@ -1,5 +1,4 @@
 //读取cookiejd数据并创建变量
-//读取cookiejd数据并创建变量
 if (String($store.get('CookieJD2', 'raw')) == "undefined" && String($store.get('CookieJD3', 'raw')) == "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw');
 if (String($store.get('CookieJD2', 'raw')) != "undefined" && String($store.get('CookieJD3', 'raw')) == "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw') + "&" + $store.get('CookieJD2', 'raw') ;
 if (String($store.get('CookieJD2', 'raw')) != "undefined" && String($store.get('CookieJD3', 'raw')) != "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw') + "&" + $store.get('CookieJD2', 'raw') + "&" + $store.get('CookieJD3', 'raw');
