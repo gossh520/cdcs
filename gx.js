0a1f3726f8718b9fa680844f2d7c1018fbc990a0
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-01 10:00:53 +0800

    Update gx.js

--- gx.js
+++ gx.js
@@ -3,3 +3,10 @@ $download('https://raw.githubusercontent.com/gossh520/cdcs/main/gx.sh', {
   folder: '/elecV2P/script/Shell',
   name: 'gx.sh'
 }).then(d=>console.log('文件已下载: ' + d)).catch(e=>console.error(e))
+//执行更新代码
+$exec('./gx.sh', {
+  cwd: './script/Shell/',
+  cb(data){
+    console.log(data)
+  }
+})
