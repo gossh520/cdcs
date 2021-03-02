c9474fa186dcb2f5b065a0da02bcd753a642cdd8
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-02 18:54:36 +0800

    Update gx.sh

--- gx.sh
+++ gx.sh
@@ -4,13 +4,3 @@ cd /elecV2P/script/Shell/scripts
 git reset --hard
 git -C /elecV2P/script/Shell/scripts pull --rebase
 wget -P /elecV2P/script/Shell/scripts -q https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js
-if [[ ${ENABLE_HANGUP} == true ]]; then
-    jdpid=$(ps -ef | grep "jd_crazy" | awk '{print $1}')
-    echo "joy进程pid=$jdpid"
-    kill $jdpid
-    echo "配置jd_crazy_joy_coin重启完成"
-    node /elecV2P/script/Shell/scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &
-    echo "启动jd_crazy_joy_coin挂机完成"
-elif [[ ${ENABLE_HANGUP} == false ]]; then
-  echo -e "没有设置变量为true  不执行joy挂机"
-fi
