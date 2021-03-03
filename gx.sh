dc11171dca31ac69fbd261d1a9ce34e58ee9964c
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-03 13:46:42 +0800

    Update gx.sh

--- gx.sh
+++ gx.sh
@@ -3,4 +3,5 @@ echo "开始执行更新仓库代码"
 cd /elecV2P/script/Shell/scripts
 git reset --hard
 git -C /elecV2P/script/Shell/scripts pull --rebase
+rm -rf /elecV2P/script/Shell/scripts/jd_try.js
 wget -P /elecV2P/script/Shell/scripts -q https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js
