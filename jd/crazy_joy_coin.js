07bb5bec197745df4398b0fc40116e0b67d36dd5
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-03 19:41:45 +0800

    Update crazy_joy_coin.js

--- jd/crazy_joy_coin.js
+++ jd/crazy_joy_coin.js
@@ -2,7 +2,7 @@
 if (String($store.get('CookieJD2', 'raw')) == "undefined" && String($store.get('CookieJD3', 'raw')) == "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw');
 if (String($store.get('CookieJD2', 'raw')) != "undefined" && String($store.get('CookieJD3', 'raw')) == "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw') + "&" + $store.get('CookieJD2', 'raw') ;
 if (String($store.get('CookieJD2', 'raw')) != "undefined" && String($store.get('CookieJD3', 'raw')) != "undefined") JD_COOKIE1 = $store.get('CookieJD', 'raw') + "&" + $store.get('CookieJD2', 'raw') + "&" + $store.get('CookieJD3', 'raw');
-$exec('node ./scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &', {
+$exec('eval $(ps -ef | grep "jd_crazy_joy_coin" | awk '{print "kill "$1}') && node ./scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &', {
   cwd: './script/Shell/',
   env: {
     JD_COOKIE: JD_COOKIE1
