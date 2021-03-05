0143994b2353be605534a4ea82ab0480067e4589
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-05 18:04:01 +0800

    Update gx.sh

--- gx.sh
+++ gx.sh
@@ -3,8 +3,8 @@ declare -i number=$RANDOM*100/32767
 echo "已设置随机延迟为 $number"
 sleep $number
 echo "开始执行更新仓库代码"
-cd /elecV2P/script/Shell/scripts
+cd /usr/local/app/script/Shell/scripts
 git reset --hard
-git -C /elecV2P/script/Shell/scripts pull --rebase
-rm -rf /elecV2P/script/Shell/scripts/jd_try.js
-wget -P /elecV2P/script/Shell/scripts -q https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js
+git -C /usr/local/app/script/Shell/scripts pull --rebase
+rm -rf /usr/local/app/script/Shell/scripts/jd_try.js
+wget -P /usr/local/app/script/Shell/scripts -q https://raw.githubusercontent.com/ZCY01/daily_scripts/main/jd/jd_try.js
