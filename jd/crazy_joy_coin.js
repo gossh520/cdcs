2e03fd04f3e8874ccf8b94397ab4e8db39e1c496
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-13 16:39:25 +0800

    Update crazy_joy_coin.js

--- jd/crazy_joy_coin.js
+++ jd/crazy_joy_coin.js
@@ -5,7 +5,8 @@ if (String($store.get('CookieJD2', 'raw')) != "undefined" && String($store.get('
 $exec('kill $(ps -ef | grep "jd_crazy" |cut -c3-5) | node ./scripts/jd_crazy_joy_coin.js >/dev/null 2>&1 &', {
   cwd: './script/Shell/',
   env: {
-    JD_COOKIE: JD_COOKIE1
+    JD_COOKIE: JD_COOKIE1,
+    BUY_JOY_LEVEL: 0
   },
   cb(data){
     console.log(data)
