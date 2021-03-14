48c999694915e922e349422143f894d3371bb74a
gossh520 <73437060+gossh520@users.noreply.github.com>
2021-03-15 02:44:02 +0800

    Update gx.sh

--- gx.sh
+++ gx.sh
@@ -8,7 +8,7 @@ git reset --hard
 echo "git pull拉取最新代码..."
 git -C /usr/local/app/script/Shell/scripts pull --rebase
 echo "npm install 安装最新依赖"
-npm install --prefix /scripts
+npm install --prefix /usr/local/app/script/Shell/scripts
 echo "开始删除和下载京东试用代码"
 rm -rf /usr/local/app/script/Shell/scripts/jd_try.js
 wget -P /usr/local/app/script/Shell/scripts -q https://raw.cnm.workers.dev/ZCY01/daily_scripts/main/jd/jd_try.js
